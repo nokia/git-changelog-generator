b3ddafb14e6c5561b3da3847698e5659bf74fc62
Waldek Maleska <waldek.maleska@nokia.com>
2018-05-22 16:03:00 +0100

    Remove the deployment provider configs
    DEB deployment is now based on curl; the provider
    is simply not reliable.
    
    Implement also the deploy_rpm and a single wrapper script
    
    Swap pypi upload as well as the provider lacks granularity
    and happily attempts to upload RPM files as well

---
+++ scripts/bintray/calc_metadata.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eEuo pipefail
+function usage {
+    cat - <<EOF
+Usage:
+
+    ${0##*/} USER API_KEY [ <USER|ORGANIZATION> [REPO_NAME]]
+EOF
+}
+
+if [[ $# -lt 2 ]]; then
+    >&2 echo "ERROR: insufficient number of input parameters"
+    usage
+    false
+fi
+
+USER=${1:-INVALID}
+API_KEY=${2:-SILLYPASS}
+SUBJECT=${3:-weakcamel}
+REPO=${3:-deb-oss}
+
+curl -X POST -u "${USER}:${API_KEY}" "https://api.bintray.com/calc_metadata/${SUBJECT}/${REPO}"
+
