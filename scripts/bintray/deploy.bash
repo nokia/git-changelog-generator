ab84281eed54aa5ce3a3a715658aaf52936ee6d5
Waldek Maleska <waldek.maleska@nokia.com>
2018-05-22 16:03:00 +0100

    Remove the deployment provider configs
    DEB deployment is now based on curl; the provider
    is simply not reliable.
    
    Implement also the deploy_rpm and a single wrapper script
    
    Swap pypi upload as well as the provider lacks granularity
    and happily attempts to upload RPM files as well

---
+++ scripts/bintray/deploy.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# set -eEuo pipefail
+
+# $1 - user
+# $2 - password
+# $3 - version
+# $4 - ignored for RPM; DEB: component
+
+# TODO: turn this into proper deploy scripts
+
+if [[ -n "${DEBUG:-}" ]]; then
+    opts="-x"
+fi
+
+dir=$(dirname "$(readlink -f "$0")")
+
+bash ${opts:-} "${dir}/deploy_rpm.bash" "$@"
+bash ${opts:-} "${dir}/deploy_deb.bash" "$@"
+
+# This in theory is not needed, bintray should should refresh
+# the repository metadata on its own. There were cases though when it didn't
+# hence an explicit request to do so
+bash ${opts:-} "${dir}/calc_metadata.bash" "$1" "$2" "$1" "deb-oss"
+bash ${opts:-} "${dir}/calc_metadata.bash" "$1" "$2" "${1}/yum-oss"
